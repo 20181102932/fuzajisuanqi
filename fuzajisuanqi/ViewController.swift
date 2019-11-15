50ee2e441bfdb0e87379797e725ffd1610ece9e5
20181102932 <908907990@qq.com>
2019-11-15 09:15:12 +0800

    123

--- fuzajisuanqi/ViewController.swift
+++ fuzajisuanqi/ViewController.swift
@@ -13,88 +13,48 @@ class ViewController: UIViewController {
     var b = ""
     var c:Double = 0
     var biaozhi:String = "1"
-    var cnt:Int = 0
-    var cntd:Int = 0
-    var cnld:Int=1
+    var cnxd:Int=0
     @IBOutlet weak var result: UITextField!
 
    
 
     @IBAction func input(_ sender: UIButton) {
-        if(cnt==0)
+        
+        if(result.text!.contains(".") && sender.currentTitle == ".")
         {
-            if(sender.currentTitle != ".")
+            return
+        }
+       // if(result.text == "0")
+      //  {
+      //      result.text = sender.currentTitle
+      //  }
+            if( result.text == "0" && sender.currentTitle != ".")
                 {
-                  result.text=result.text!+sender.currentTitle!
-                    cnt = 1
+                  result.text=sender.currentTitle!
                 }
-            if(cnld>0 && sender.currentTitle=="0")
-            {
-                result.text=result.text!+sender.currentTitle!
-                cnld=cnld-1
+            else{
+                result.text=result.text! + sender.currentTitle!
             }
-        }
-        if(cnt > 1)
-        {
-            if(sender.currentTitle != "0")
-            {
-                cnld=0
-            }
-            if(cnld>0 && sender.currentTitle=="0")
-            {
-                 result.text=result.text!+sender.currentTitle!
-                cnld=cnld-1
-            }
-            if(sender.currentTitle != ".")
-            {
-                result.text=result.text!+sender.currentTitle!
-                cnt=cnt+1
-            }
-            if(sender.currentTitle==".")
-            {
-                if(cntd==0)
-                {
-                    result.text=result.text!+sender.currentTitle!
-                    cntd=cntd+1
-                }
-            }
-        }
-        if(result.text != "")
-        {
-         cnt=cnt+1
-        }
     }
     @IBAction func jia(_ sender: UIButton) {
         biaozhi = "1"
         a = result.text!
-        result.text = ""
-        cntd=0
-        cnt=0
-        cnld = 1
+        result.text = "0"
     }
     @IBAction func jian(_ sender: UIButton) {
         biaozhi = "2"
          a = result.text!
-        result.text = ""
-        cntd=0
-        cnt=0
-        cnld = 1
+        result.text = "0"
     }
     @IBAction func cheng(_ sender: UIButton) {
         biaozhi = "3"
          a = result.text!
-        result.text = ""
-        cntd=0
-        cnt=0
-        cnld = 1
+        result.text = "0"
     }
     @IBAction func chu(_ sender: UIButton) {
         biaozhi = "4"
         a = result.text!
-        result.text = ""
-        cntd=0
-        cnt=0
-        cnld = 1
+        result.text = "0"
     }
     @IBAction func dengyu(_ sender: UIButton) {
         b = result.text!
@@ -118,15 +78,10 @@ class ViewController: UIViewController {
             c = Double(a)! / Double(b)!
             result.text = String(c)
         }
-        cntd=0
-        cnt=0
-        cnld = 1
+       
     }
     @IBAction func qingchu(_ sender: UIButton) {
-        result.text = ""
-        cntd=0
-        cnt=0
-        cnld = 1
+        result.text = "0"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
